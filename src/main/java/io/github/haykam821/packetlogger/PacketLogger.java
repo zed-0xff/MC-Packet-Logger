001f1f98e21ab02bf02dd431ceacf14dfea32009
Andrey "Zed" Zaikin <andrey.zaikin@leadfeeder.com>
2022-07-19 21:00:01 +0300

    add mappings reading for class names

--- src/main/java/io/github/haykam821/packetlogger/PacketLogger.java
+++ src/main/java/io/github/haykam821/packetlogger/PacketLogger.java
@@ -19,14 +19,31 @@ import net.fabricmc.api.ModInitializer;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import net.fabricmc.mappingio.MappingReader;
+import net.fabricmc.mappingio.tree.MappingTree;
+import net.fabricmc.mappingio.tree.MappingTree.ClassMapping;
+import net.fabricmc.mappingio.tree.MemoryMappingTree;
+import java.util.Arrays;
+
 public class PacketLogger implements ModInitializer {
 	public static final String MOD_ID = "Packet Logger";
 	private static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     public static ModConfig CONFIG = null;
+    private static MemoryMappingTree mappings;
+    private static int nsI; // intermediary
+    private static int nsN; // named
 
     @Override
 	public void onInitialize() {
 		CONFIG = AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new).getConfig();
+        if ( CONFIG.useMappings ) {
+            initMappings();
+        }
 	}
 
 	private static Identifier getChannel(Packet<?> packet) {
@@ -66,7 +83,7 @@ public class PacketLogger implements ModInitializer {
 			return;
 		}
 
-		LOGGER.info("Sending packet with name '{}' ({}) / data {}", packet.getClass().getName(), sideName, data);
+		LOGGER.info("SEND '{}' ({}) / data {}", unmap(packet.getClass().getName()), sideName, data);
 	}
 
 	public static void logReceivedPacket(Packet<?> packet, NetworkSide side) {
@@ -90,11 +107,11 @@ public class PacketLogger implements ModInitializer {
 			return;
 		}
 
-		LOGGER.info("Received packet with name '{}' ({}) / data {}", packet.getClass().getName(), sideName, data);
+		LOGGER.info("RECV '{}' ({}) / data {}", unmap(packet.getClass().getName()), sideName, data);
 	}
 
 	public static void logReceivedPacket(ByteBuf buffer) {
-        if ( CONFIG == null || !CONFIG.enabled ) return;
+        if ( CONFIG == null || !CONFIG.enabled || !CONFIG.logHex ) return;
 		StringBuilder data = new StringBuilder();
 		buffer.markReaderIndex();
 		char[] hex = "0123456789ABCDEF".toCharArray();
@@ -109,4 +126,42 @@ public class PacketLogger implements ModInitializer {
 		
 		LOGGER.info("Received packet HEX: {}", data.toString());
 	}
+
+    private static void initMappings() {
+        String path = CONFIG.mapPath.replaceFirst("^~", System.getProperty("user.home"));
+        mappings = readMappings(Paths.get(path));
+        if ( mappings != null ) {
+            LOGGER.info("got " + mappings.getClasses().size() + " classes from " + path + " with namespaces " + mappings.getDstNamespaces());
+            nsI = mappings.getDstNamespaces().indexOf("intermediary");
+            nsN = mappings.getDstNamespaces().indexOf("named");
+
+            mappings.setIndexByDstNames(true);
+        }
+    }
+
+    private static MemoryMappingTree readMappings(Path input) {
+        try (BufferedReader reader = Files.newBufferedReader(input)) {
+            MemoryMappingTree mappingTree = new MemoryMappingTree();
+            MappingReader.read(reader, mappingTree);
+
+            return mappingTree;
+        } catch (IOException e) {
+            LOGGER.error("Failed to read mappings: ", e);
+        }
+        return null;
+    }
+
+    private static String unmap(String src) {
+        if ( !CONFIG.useMappings )
+            return src;
+        if ( mappings == null )
+            return src;
+        ClassMapping c = mappings.getClass(src.replace(".","/"), nsI);
+        if ( c == null )
+            return src;
+        String dst = c.getDstName(nsN);
+        if ( dst == null )
+            return src;
+        return dst;
+    }
 }
