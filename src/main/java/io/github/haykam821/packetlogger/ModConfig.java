896a3838f7719ca2e669fdde21f85930ae747d9b
Andrey "Zed" Zaikin <andrey.zaikin@leadfeeder.com>
2022-07-21 15:38:47 +0300

    better code

--- src/main/java/io/github/haykam821/packetlogger/ModConfig.java
+++ src/main/java/io/github/haykam821/packetlogger/ModConfig.java
@@ -20,10 +20,41 @@ public class ModConfig implements ConfigData {
     boolean useMappings = false;
     String  mapPath = "~/.gradle/caches/fabric-loom/1.19/net.fabricmc.yarn.1_19.1.19+build.4-v2/mappings.tiny";
 
-    public List<String> ignores = Arrays.asList(
+    List<String> stripPrefixes = Arrays.asList(
+        "net/minecraft/network/packet/s2c/play/",
+        "net/minecraft/network/packet/c2s/play/"
+        );
+
+    List<String> ignores = Arrays.asList(
+            "BlockBreakingProgressS2CPacket",
+            "ChunkDataS2CPacket",
+            "ChunkDeltaUpdateS2CPacket",
+            "EntityAnimationS2CPacket",
+            "EntityEquipmentUpdateS2CPacket",
+            "EntityPositionS2CPacket",
+            "EntityS2CPacket$MoveRelative",
+            "EntityS2CPacket$Rotate",
+            "EntityS2CPacket$RotateAndMoveRelative",
+            "EntitySetHeadYawS2CPacket",
+            "EntityTrackerUpdateS2CPacket",
+            "EntityVelocityUpdateS2CPacket",
+            "GameMessageS2CPacket",
+            "HandSwingC2SPacket",
+            "HealthUpdateS2CPacket",
             "KeepAliveC2SPacket",
             "KeepAliveS2CPacket",
+            "ParticleS2CPacket",
             "PlayPingS2CPacket",
-            "PlayPongC2SPacket"
+            "PlayPongC2SPacket",
+            "PlaySoundIdS2CPacket",
+            "PlayerListHeaderS2CPacket",
+            "PlayerListS2CPacket",
+            "PlayerMoveC2SPacket$Full",
+            "PlayerMoveC2SPacket$LookAndOnGround",
+            "PlayerMoveC2SPacket$PositionAndOnGround",
+            "ScoreboardObjectiveUpdateS2CPacket",
+            "ScoreboardPlayerUpdateS2CPacket",
+            "TeamS2CPacket",
+            "WorldTimeUpdateS2CPacket"
             );
 }
--- src/main/java/io/github/haykam821/packetlogger/PacketLogger.java
+++ src/main/java/io/github/haykam821/packetlogger/PacketLogger.java
@@ -1,6 +1,8 @@
 package io.github.haykam821.packetlogger;
 
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.HashMap;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,6 +12,7 @@ import io.github.haykam821.packetlogger.mixin.CustomPayloadS2CPacketAccessor;
 import io.netty.buffer.ByteBuf;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import net.minecraft.network.NetworkSide;
 import net.minecraft.network.Packet;
 import net.minecraft.util.ActionResult;
@@ -28,8 +31,12 @@ import java.nio.file.Paths;
 import net.fabricmc.mappingio.MappingReader;
 import net.fabricmc.mappingio.tree.MappingTree;
 import net.fabricmc.mappingio.tree.MappingTree.ClassMapping;
+import net.fabricmc.mappingio.tree.MappingTree.FieldMapping;
 import net.fabricmc.mappingio.tree.MemoryMappingTree;
-import java.util.Arrays;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import com.google.common.collect.ObjectArrays;
 
 public class PacketLogger implements ModInitializer {
 	public static final String MOD_ID = "Packet Logger";
@@ -38,11 +45,14 @@ public class PacketLogger implements ModInitializer {
     private static MemoryMappingTree mappings;
     private static int nsI; // intermediary
     private static int nsN; // named
+    private static HashMap<String, UnmapperProxy> unmappersCache = new HashMap<String, UnmapperProxy>();
 
     @Override
 	public void onInitialize() {
 		CONFIG = AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new).getConfig();
         AutoConfig.getConfigHolder(ModConfig.class).registerSaveListener((manager, data) -> {
+            LOGGER.info("PacketLogger ignores: " + CONFIG.ignores.toString());
+            unmappersCache.clear();
             if ( data.useMappings ) {
                 initMappings();
             }
@@ -63,64 +73,136 @@ public class PacketLogger implements ModInitializer {
 	}
 
 	private static String getSideName(NetworkSide side) {
-		if (side == NetworkSide.CLIENTBOUND) return "client";
-		if (side == NetworkSide.SERVERBOUND) return "server";
+		if (side == NetworkSide.CLIENTBOUND) return "C";
+		if (side == NetworkSide.SERVERBOUND) return "S";
 
 		return side.name().toLowerCase(Locale.ROOT);
 	}
 
-	public static void logSentPacket(Packet<?> packet, NetworkSide side) {
-        if ( CONFIG == null || !CONFIG.logSent ) return;
-		String sideName = PacketLogger.getSideName(side);
-        String data = "";
-
-        Field[] allFields = packet.getClass().getDeclaredFields();
-        for (Field field : allFields) {
-            field.setAccessible(true);
-            try {
-                data += "f: " + field.getName() + "; d: " + field.get(packet) + " / ";
-            }catch(IllegalAccessException ex) {
-                data += "f: " + field.getName() + "; exception";
+    private static String stripPrefixes(String s) {
+        for ( String prefix : CONFIG.stripPrefixes ) {
+            if (s.startsWith(prefix)) {
+                return s.substring(prefix.length());
             }
         }
-                
+        return s;
+    }
+
+    // from https://github.com/dancerjohn/LibEx/blob/master/libex/src/main/java/org/libex/reflect/ReflectionUtils.java
+    public static Field[] getFieldsUpTo(@Nonnull Class<?> type, @Nullable Class<?> exclusiveParent) {
+        Field[] result = type.getDeclaredFields();
+
+        Class<?> parentClass = type.getSuperclass();
+        if (parentClass != null && (exclusiveParent == null || !parentClass.equals(exclusiveParent))) {
+            Field[] parentClassFields = getFieldsUpTo(parentClass, exclusiveParent);
+            result = ObjectArrays.concat(result, parentClassFields, Field.class);
+        }
+
+        return result;
+    }
+
+    private static UnmapperProxy getUnmapperProxy(String className) {
+        UnmapperProxy um = unmappersCache.get(className);
+        if ( um != null )
+            return um;
+        um = new UnmapperProxy(className);
+        unmappersCache.put(className, um);
+        return um;
+    }
+
+    private static String guessFieldName(Field field) {
+        UnmapperProxy um = getUnmapperProxy( field.getDeclaringClass().getName() );
+        return um.fieldName(field.getName());
+    }
+
+    private static String objectToString(Object object) {
+        if ( object == null || object instanceof java.lang.Enum )
+            return String.valueOf(object);
+
+        UnmapperProxy um = getUnmapperProxy(object.getClass().getName());
+        if ( !um.isMapped() )
+            return object.toString();
+
+//        LOGGER.info("[d] " + object.getClass().getName() + " -> " + um.className());
+//
+//        if ( um != null )
+//            return object.toString();
+
+        String data = "";
+        boolean first = true;
+        try {
+            Field[] allFields = getFieldsUpTo(object.getClass(), Object.class);
+            for (Field field : allFields) {
+                if ( Modifier.isStatic(field.getModifiers() )) continue;
+                if ( first ) first = false; else data += ", ";
+                field.setAccessible(true);
+                try {
+                    data += guessFieldName(field) + "=" + field.get(object);
+                }catch(IllegalAccessException ex) {
+                    data += guessFieldName(field) + "=<exception!>";
+                }
+            }
+        } catch (java.lang.reflect.InaccessibleObjectException e) {
+            LOGGER.info("[?] InaccessibleObjectException: " + object.getClass().getName() + " -> " + um.className());
+            return String.valueOf(object);
+        }
+
+        return String.format("%s{ %s }", stripPrefixes(um.className()), data);
+    }
+
+    private static String packetToString(Packet<?> packet) {
+        UnmapperProxy um = getUnmapperProxy(packet.getClass().getName());
+        if ( um.isIgnored() )
+            return null;
+
+        String data = "";
+        boolean first = true;
+
+        Field[] allFields = getFieldsUpTo(packet.getClass(), Object.class);
+        for (Field field : allFields) {
+            if ( Modifier.isStatic(field.getModifiers() )) continue;
+            if ( first ) first = false; else data += ", ";
+            field.setAccessible(true);
+            try {
+                data += guessFieldName(field) + "=" + objectToString(field.get(packet));
+            }catch(IllegalAccessException ex) {
+                data += guessFieldName(field) + "=<exception!>";
+            }
+        }
+
+        return String.format("%s{ %s }", stripPrefixes(um.className()), data);
+    }
+
+	public static void logSentPacket(Packet<?> packet, NetworkSide side) {
+        if ( CONFIG == null || !CONFIG.logSent ) return;
+
+        String data = packetToString(packet);
+        if ( data == null ) return;
+
+		String sideName = PacketLogger.getSideName(side);
 		Identifier channel = PacketLogger.getChannel(packet);
 		if (channel != null) {
 			LOGGER.info("Sending packet with channel '{}' ({}) / data {}", channel, sideName, data);
 			return;
 		}
 
-        String className = unmapOrIgnore(packet.getClass().getName());
-        if ( className == null )
-            return;
-		LOGGER.info("SEND '{}' ({}) / data {}", className, sideName, data);
+		LOGGER.info("{} SEND {}", sideName, data);
 	}
 
 	public static void logReceivedPacket(Packet<?> packet, NetworkSide side) {
         if ( CONFIG == null || !CONFIG.logReceived ) return;
-		String sideName = PacketLogger.getSideName(side);
-		String data = "";
-		
-        Field[] allFields = packet.getClass().getDeclaredFields();
-        for (Field field : allFields) {
-            field.setAccessible(true);
-            try {
-                data += "f: " + field.getName() + "; d: " + field.get(packet) + " / ";
-            }catch(IllegalAccessException ex) {
-                data += "f: " + field.getName() + "; exception";
-            }
-        }
 
+        String data = packetToString(packet);
+        if ( data == null ) return;
+
+		String sideName = PacketLogger.getSideName(side);
 		Identifier channel = PacketLogger.getChannel(packet);
 		if (channel != null) {
 			LOGGER.info("Received packet with channel '{}' ({}) / data {}", channel, sideName, data);
 			return;
 		}
 
-        String className = unmapOrIgnore(packet.getClass().getName());
-        if ( className == null )
-            return;
-		LOGGER.info("RECV '{}' ({}) / data {}", className, sideName, data);
+		LOGGER.info("{} RECV {}", sideName, data);
 	}
 
 	public static void logReceivedPacket(ByteBuf buffer) {
@@ -164,26 +246,54 @@ public class PacketLogger implements ModInitializer {
         return null;
     }
 
-    private static String unmapOrIgnore(String src) {
-        String className = unmap(src);
-        for ( String suffix : CONFIG.ignores ) {
-            if ( className.endsWith(suffix) )
-                return null;
-        }
-        return className;
-    }
+    static class UnmapperProxy {
+        private String dstClassName;
+        private ClassMapping classMapping = null;
+        private boolean isMapped = false;
 
-    private static String unmap(String src) {
-        if ( !CONFIG.useMappings )
-            return src;
-        if ( mappings == null )
-            return src;
-        ClassMapping c = mappings.getClass(src.replace(".","/"), nsI);
-        if ( c == null )
-            return src;
-        String dst = c.getDstName(nsN);
-        if ( dst == null )
-            return src;
-        return dst;
+        UnmapperProxy(String srcClassName) {
+            if ( !CONFIG.useMappings || mappings == null ) {
+                dstClassName = srcClassName;
+                return;
+            }
+            classMapping = mappings.getClass(srcClassName.replace(".","/"), nsI);
+            if ( classMapping == null ) {
+                dstClassName = srcClassName;
+                return;
+            }
+            String dst = classMapping.getDstName(nsN);
+            if ( dst == null ) {
+                dstClassName = srcClassName;
+                return;
+            }
+            isMapped = true;
+            dstClassName = dst;
+        }
+
+        boolean isMapped() {
+            return isMapped;
+        }
+
+        boolean isIgnored() {
+            for ( String suffix : CONFIG.ignores ) {
+                if ( dstClassName.endsWith(suffix) )
+                    return true;
+            }
+            return false;
+        }
+
+        String className() {
+            return dstClassName;
+        }
+
+        String fieldName(String srcFieldName) {
+            if ( classMapping == null )
+                return srcFieldName;
+            FieldMapping fieldMapping = classMapping.getField(srcFieldName, null /* desc? */, nsI);
+            if ( fieldMapping == null )
+                return srcFieldName;
+            String dst = fieldMapping.getDstName(nsN);
+            return (dst == null) ? srcFieldName : dst;
+        }
     }
 }
